7c6221bbda60c0350d22e1f9db20583310be12f8
Yuvaraj Chenniappan <yuvaraj@Yuvarajs-MacBook-Pro.local>
2025-11-28 00:02:29 +0530

    Test Commit

---
+++ app/pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1 class="text-5xl text-red-500">Welcome to the Index Page megaPh</h1>
+  </div>
+</template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
+  compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/image', '@nuxt/ui']
-})
+  css: ["./app/assets/css/main.css"],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  modules: ["@nuxt/eslint", "@nuxt/image", "@nuxt/ui"],
+});
