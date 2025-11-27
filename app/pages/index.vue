4e1d0655772148fbabb1b29214c9a5983d1913d3
Yuvaraj Chenniappan <yuvaraj@Yuvarajs-MacBook-Pro.local>
2025-11-28 00:58:58 +0530

    Test Commit 5

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,44 +1,5 @@
 <template>
   <div>
-    <h1 class="text-5xl text-green-500">Welcome to the Index Page megaPh</h1>
-    <p class="text-sky-500">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam veniam,
-      reiciendis laborum sequi nostrum aliquam ad et quam obcaecati sint quidem
-      odit dolore vero perferendis amet cupiditate accusantium quod sunt
-      placeat. Molestias autem neque modi fugiat quis culpa cumque iste,
-      cupiditate enim necessitatibus temporibus mollitia voluptas perspiciatis
-      adipisci recusandae hic ipsum, tempore dignissimos magni odit repudiandae.
-      Animi, unde iure? Excepturi vitae, recusandae aspernatur quibusdam
-      laboriosam explicabo voluptates nulla. Mollitia, magni pariatur dicta
-      distinctio sint ab adipisci debitis eius nostrum possimus aut et porro
-      molestiae est eos sequi voluptatum? Officiis, quo incidunt. Tenetur sit,
-      minus et blanditiis aperiam sint. Cum, ab?
-    </p>
-    <p class="text-fuchsia-500">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam veniam,
-      reiciendis laborum sequi nostrum aliquam ad et quam obcaecati sint quidem
-      odit dolore vero perferendis amet cupiditate accusantium quod sunt
-      placeat. Molestias autem neque modi fugiat quis culpa cumque iste,
-      cupiditate enim necessitatibus temporibus mollitia voluptas perspiciatis
-      adipisci recusandae hic ipsum, tempore dignissimos magni odit repudiandae.
-      Animi, unde iure? Excepturi vitae, recusandae aspernatur quibusdam
-      laboriosam explicabo voluptates nulla. Mollitia, magni pariatur dicta
-      distinctio sint ab adipisci debitis eius nostrum possimus aut et porro
-      molestiae est eos sequi voluptatum? Officiis, quo incidunt. Tenetur sit,
-      minus et blanditiis aperiam sint. Cum, ab?
-    </p>
-    <p class="text-yellow-500">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam veniam,
-      reiciendis laborum sequi nostrum aliquam ad et quam obcaecati sint quidem
-      odit dolore vero perferendis amet cupiditate accusantium quod sunt
-      placeat. Molestias autem neque modi fugiat quis culpa cumque iste,
-      cupiditate enim necessitatibus temporibus mollitia voluptas perspiciatis
-      adipisci recusandae hic ipsum, tempore dignissimos magni odit repudiandae.
-      Animi, unde iure? Excepturi vitae, recusandae aspernatur quibusdam
-      laboriosam explicabo voluptates nulla. Mollitia, magni pariatur dicta
-      distinctio sint ab adipisci debitis eius nostrum possimus aut et porro
-      molestiae est eos sequi voluptatum? Officiis, quo incidunt. Tenetur sit,
-      minus et blanditiis aperiam sint. Cum, ab?
-    </p>
+    <h1 class="text-5xl text-amber-500">Welcome to the megaPh page</h1>
   </div>
 </template>
